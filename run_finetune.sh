00bb256aee075405b080ff4f620a544dce97f765
andyple <andy2639le@gmail.com>
2026-04-01 20:27:37 -0500

    added slurm script to run finetuning

---
+++ run_finetune.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=py
+#SBATCH --output=py_output.out
+#SBATCH --error=py_error.out
+#SBATCH --time=2-00:00:00
+#SBATCH --partition=gpu
+#SBATCH --gpus=volta:4
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+
+module add python
+
+# Run python script
+/project/pakhrin/venv/bin/python /project/pakhrin/simonsha/qwen3-vl-lora/qwen3-vl-lora.py --r 32 --batch_size 2 
